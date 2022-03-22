76423b6c100ece5f28fd12106a1700e160bbabb5
zekesiegman <eas14751@uga.edu>
2022-03-22 10:36:47 -0400

    models are working with foreign keys

--- cinemaE-Booking/home/models.py
+++ cinemaE-Booking/home/models.py
@@ -4,26 +4,6 @@ from django.contrib.auth.models import User as us
 # Create your models here.
 
 
-class UserStatus(models.Model):
-    userStatusID = models.IntegerField(primary_key=True)
-    status = models.CharField(max_length=45)
-
-
-class UserType(models.Model):
-    userTypeID = models.IntegerField(primary_key=True)
-    userTypeName = models.CharField(max_length=45)
-
-
-class Account(models.Model):
-    accountID = models.IntegerField(primary_key=True)
-    carNo = models.IntegerField()
-    userID = models.IntegerField()
-    type = models.CharField(max_length=45)
-    expirationDate = models.DateField()
-    billingAdd = models.CharField(max_length=45)
-    # user_userID = models.ForeignKey('User', on_delete=models.CASCADE)
-
-
 class User(models.Model):
     # userObj = models.OneToOneField(us, on_delete=models.CASCADE)
     userID = models.IntegerField(primary_key=True, default=1)
@@ -34,11 +14,31 @@ class User(models.Model):
     phone = models.IntegerField()
     status = models.IntegerField()
     enrollForPromotions = models.CharField(max_length=45)
-    userStatus = models.OneToOneField(UserStatus, on_delete=models.CASCADE)
-    userType = models.OneToOneField(UserType, on_delete=models.CASCADE)
+
+
+class UserStatus(models.Model):
+    userStatusID = models.IntegerField(primary_key=True)
+    status = models.CharField(max_length=45)
+    user_userID = models.ForeignKey('User', on_delete=models.CASCADE, default=1)
+
+
+class UserType(models.Model):
+    userTypeID = models.IntegerField(primary_key=True)
+    userTypeName = models.CharField(max_length=45)
+    user_userID = models.ForeignKey('User', on_delete=models.CASCADE, default=1)
+
+
+class Account(models.Model):
+    accountID = models.IntegerField(primary_key=True, default=1)
+    carNo = models.IntegerField()
+    userID = models.IntegerField()
+    type = models.CharField(max_length=45)
+    expirationDate = models.DateField()
+    billingAdd = models.CharField(max_length=45)
+    user_userID = models.ForeignKey('User', on_delete=models.CASCADE, default=1)
 
 
 class CardType(models.Model):
     cardTypeID = models.IntegerField(primary_key=True)
     type = models.CharField(max_length=45)
-    account = models.OneToOneField(Account, on_delete=models.CASCADE)
+    account_accountID = models.ForeignKey('Account', on_delete=models.CASCADE, default=1)
