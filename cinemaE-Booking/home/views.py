3d9e34f558a4618090b33f367f83fe618fb55665
zekesiegman <eas14751@uga.edu>
2022-05-06 10:41:25 -0400

    final demo

--- cinemaE-Booking/home/views.py
+++ cinemaE-Booking/home/views.py
@@ -431,8 +431,9 @@ def checkout(request):
             order = Order.objects.get(showtimeID=showtime, userID=user)
             order.total = total
             order.save()
+            cardApplied = True
             context = {'showtime': showtime, 'movie': movie, 'tickets': tickets, 'seatprices': seatPrices,
-                       'tax': tax, 'total': total, 'accounts': accounts}
+                       'tax': tax, 'total': total, 'accounts': accounts, 'cardApplied': cardApplied}
             return render(request, '../templates/checkout.html', context)
     return render(request, '../templates/checkout.html', context)
 
